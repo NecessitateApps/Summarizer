dabefd35a1b2a185bb33ee959795da8a77689a4c
bisherkudaimi <bisher.kudaimi@gmail.com>
2018-10-13 21:39:27 -0700

    After combining C + N files
    Topic needs to be fixed still

--- main.py
+++ main.py
@@ -1,58 +1,114 @@
-# from sumy.parsers.plaintext import PlaintextParser
+from flask import Flask, jsonify, request
+import requests
 from sumy.parsers.html import HtmlParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+from gensim.summarization import keywords
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import string
+import nltk
+from goose3 import Goose
+import heapq
 
-url = 'https://www.cnn.com/2018/10/10/politics/steele-dossier-letter/index.html'
-parser = HtmlParser.from_url(url, Tokenizer("english"))
-summarizer = LexRankSummarizer()
+app = Flask(__name__)
 
-summary = summarizer(parser.document, 3)
+def summarize(url):
+    g = Goose()
+    article = g.extract(url=url)
+    clean = article.cleaned_text
+    stopword_set = set(stopwords.words("english"))
+    sentence_list = nltk.sent_tokenize(clean)
 
-for sentence in summary:
-    print(sentence)
+    word_frequencies = {}
+    for word in nltk.word_tokenize(clean):
+        if word not in stopword_set:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+
+    maximum_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    summary_sentences = heapq.nlargest(4, sentence_scores, key=sentence_scores.get)
+
+    summary = ' '.join(summary_sentences)
+
+    return summary
+    # print('Summary:', summary)
+
+def get_topic(url):
+    g = Goose()
+    article = g.extract(url=url)
+    title = article.title.upper()
+    clean = article.cleaned_text
+    rando = '’' + 'The' + '”' +'“' +'Â' + 'â'
+    clean_lower = clean.lower()
+
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(clean)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    filtered_sentence = ""
+    for w in word_tokens:
+        if w not in stop_words and w not in string.punctuation and w not in rando:
+            filtered_sentence += (str(w) + " ")
+    final_count_words = Counter(filtered_sentence.split())
+
+    return final_count_words
+    # print(final_count_words)
 
 
-# from lexrank import STOPWORDS, LexRank
-# from pathlib import Path
-#
-# documents = []
-# directory = Path('bbc/politics')
-#
-# for file_path in directory.glob('*.txt'):
-#     with file_path.open(mode='rt', encoding='utf-8') as fp:
-#         documents.append(fp.readlines())
-# print(documents)
-# lxr = LexRank(documents, stopwords=STOPWORDS['en'])
-#
-# sentences = [
-#     'One of David Cameron\'s closest friends and Conservative allies, '
-#     'George Osborne rose rapidly after becoming MP for Tatton in 2001.',
-#
-#     'Michael Howard promoted him from shadow chief secretary to the '
-#     'Treasury to shadow chancellor in May 2005, at the age of 34.',
-#
-#     'Mr Osborne took a key role in the election campaign and has been at '
-#     'the forefront of the debate on how to deal with the recession and '
-#     'the UK\'s spending deficit.',
-#
-#     'Even before Mr Cameron became leader the two were being likened to '
-#     'Labour\'s Blair/Brown duo. The two have emulated them by becoming '
-#     'prime minister and chancellor, but will want to avoid the spats.',
-#
-#     'Before entering Parliament, he was a special adviser in the '
-#     'agriculture department when the Tories were in government and later '
-#     'served as political secretary to William Hague.',
-#
-#     'The BBC understands that as chancellor, Mr Osborne, along with the '
-#     'Treasury will retain responsibility for overseeing banks and '
-#     'financial regulation.',
-#
-#     'Mr Osborne said the coalition government was planning to change the '
-#     'tax system \"to make it fairer for people on low and middle '
-#     'incomes\", and undertake \"long-term structural reform\" of the '
-#     'banking sector, education and the welfare state.',
-# ]
-#
-# summary_cont = lxr.get_summary(sentences, threshold=None)
-# print(summary_cont)
+def make_json():
+    url_req = requests.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=4447d852b5264e1dab136e3f15d4089c')
+    url_dict = url_req.json()
+    final_dict = {}
+    final_dict['articles'] = []
+
+    for i in range(10):
+        temp_dict = {}
+        article_dict = url_dict['articles'][i]
+
+        url = article_dict['url']
+
+        temp_dict['title'] = article_dict['title']
+        temp_dict['topic'] = get_topic(url)
+        temp_dict['url'] = url
+        temp_dict['summary'] = summarize(url)
+
+        publisher = article_dict['source']['name']
+        if "." in publisher:
+            publisher = publisher.split('.')[0]
+
+        temp_dict['image'] = article_dict['urlToImage']
+        temp_dict['source'] = publisher
+
+        final_dict['articles'].append(temp_dict)
+
+    return final_dict
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json}), 201
+    else:
+        finished_dict = make_json()
+        return jsonify(finished_dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
